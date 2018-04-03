dc49fec45939f692b27e30b6e6f7e9edf455eb2d
Roberts Krisjanis Plorins <cros1404@gmail.com>
2018-04-03 08:52:44 +0300

    improvements

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,15 +2,17 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
 use App\Controller\MapController;
 
 class DefaultController extends Controller
 {
     /**
-    * @Route("/")
+    * @Route("/", name="index")
     */
     public function index()
     {
@@ -22,7 +24,7 @@ class DefaultController extends Controller
     */
     public function geocode(Request $request)
     {
-        $address = $request->request->get('address');
+        $address = $request->query->get('address');
         if (!$address) 
             return $this->render('geocode_form.html.twig');
         else {
@@ -35,27 +37,82 @@ class DefaultController extends Controller
     /**
     * @Route("/place_marker")
     */
-    public function place_marker(Request $request)
+    public function place_marker(Request $request,  SessionInterface $session)
     {
-        $type = $request->request->get('type');
-        $icon = $request->request->get('icon');
-        $color = $request->request->get('color');
-        $iconOption = $icon ? "img/{$color}/{$icon}"
-            : "http://maps.google.com/mapfiles/ms/icons/{$color}-dot.png";
+        $type = $request->query->get('type');
+        $iconOption = $session->get('iconOption');
         if (!$type) 
             return $this->render('place_marker_form.html.twig');
         else if ($type == "address") {
-            $address = $request->request->get('address');
+            $address = $request->query->get('address');
             $geocode = MapController::geocode($address);
             return $this->render('place_marker.html.twig', 
-                array("lat" => $geocode->lat, "lng" => $geocode->lng,
-                      "API_KEY" => "AIzaSyA2utCgFATmbG4fvzdHpFXvXErUYSJodwc",
+                array("lat"        => $geocode->lat, 
+                      "lng"        => $geocode->lng,
+                      "API_KEY"    => "AIzaSyA2utCgFATmbG4fvzdHpFXvXErUYSJodwc",
                       "iconOption" => $iconOption));
         } else if ($type == "geocode")
             return $this->render('place_marker.html.twig', 
-                array("lat" => $request->request->get('lat'),
-                      "lng" => $request->request->get('lng'),
-                      "API_KEY" => "AIzaSyA2utCgFATmbG4fvzdHpFXvXErUYSJodwc",
+                array("lat"        => $request->query->get('lat'),
+                      "lng"        => $request->query->get('lng'),
+                      "API_KEY"    => "AIzaSyA2utCgFATmbG4fvzdHpFXvXErUYSJodwc",
                       "iconOption" => $iconOption));
     }
+
+    /**
+    * @Route("/custom_marker")
+    */
+    public function custom_marker(Request $request, SessionInterface $session) {
+        $icon = $request->request->get('icon');
+        $color = $request->request->get('color');
+        if ($icon === null) 
+            return $this->render('custom_marker.html.twig');
+        else {
+            $iconOption = $icon ? "img/{$color}/{$icon}"
+                : "http://maps.google.com/mapfiles/ms/icons/{$color}-dot.png";
+            $session->set('iconOption', $iconOption);
+            return $this->redirectToRoute("index");
+        }
+    }
+
+    /**
+    * @Route("/distance")
+    */
+    public function distance(Request $request, SessionInterface $session) {
+        $iconOption = $session->get('iconOption');
+        $lat1 = $request->query->get('lat1');
+        $lng1 = $request->query->get('lng1');
+        $lat2 = $request->query->get('lat2');
+        $lng2 = $request->query->get('lng2');
+        if ($lat1 === null || $lat2 === null) {
+            return $this->render('distance.html.twig', 
+                array("API_KEY"    => "AIzaSyA2utCgFATmbG4fvzdHpFXvXErUYSJodwc",
+                      "iconOption" => $iconOption));
+        } else {
+            return $this->render('distance_result.html.twig', 
+                array("result" => MapController::vincentyGreatCircleDistance(
+                    $lat1, $lng1, $lat2, $lng2)));
+        }
+    }
+
+    /**
+    * @Route("/directions")
+    */
+    public function directions(Request $request, SessionInterface $session) {
+        $iconOption = $session->get('iconOption');
+        $lat1 = $request->query->get('lat1');
+        $lng1 = $request->query->get('lng1');
+        $lat2 = $request->query->get('lat2');
+        $lng2 = $request->query->get('lng2');
+        if ($lat1 === null || $lat2 === null) {
+            return $this->render('directions.html.twig', 
+                array("API_KEY"    => "AIzaSyA2utCgFATmbG4fvzdHpFXvXErUYSJodwc",
+                      "iconOption" => $iconOption));
+        } else {
+            return $this->render('directions_result.html.twig', 
+                array("directions" => MapController::getDirections(
+                    $lat1, $lng1, $lat2, $lng2)));
+        }
+    }
+
 }
